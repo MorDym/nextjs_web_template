02cb83129db865f2683002fd546ee9aa984c79bb
Matěj Košín <103522369+MorDym@users.noreply.github.com>
2025-08-19 14:52:59 +0200

    feat: basic content optimized

--- nextjs_web_template/src/app/about/page.tsx
+++ nextjs_web_template/src/app/about/page.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 import Accordion from '../components/Accordion'
 import { accordionData } from '../data/data'
 import BasicContent from '../components/BasicContent';
+import { basicContentData } from '../data/data';
 
 export default function about() {
   return (
-           <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-        <BasicContent />
+    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <div className='flex flex-col sm:flex-row gap-5'>
+        {basicContentData.map((item) => {
+          const { id, title, content, img, seznam } = item;
+          return (
+            <BasicContent key={id} id={id} title={title} content={content} img={img} seznam={seznam} />
+          );
+        })}
+      </div>
       <div className='flex flex-col gap-1'>
         {accordionData.map((accord) => {
           const { id, title, content } = accord;
--- nextjs_web_template/src/app/components/BasicContent.tsx
+++ nextjs_web_template/src/app/components/BasicContent.tsx
@@ -1,32 +1,30 @@
 import React from 'react'
-import { basicContentData } from '../data/data'
 import Image from 'next/image';
+import { BasicContentProps } from '../data/type';
 
-function BasicContent() {
+function BasicContent({ id, title, content, img, seznam }: BasicContentProps) {
+
     return (
-        <div className='flex flex-col gap-5'>
-            {basicContentData.map((contents) => {
-                const { id, title, content, img, seznam } = contents;
-                return (
-                    <div key={id} className="flex flex-col sm:flex-row gap-5">
-                        <Image src={img} alt={title} width={350} height={350} />
-                        <div>
-                            <h2>{title}</h2>
-                            <p>{content}</p>
-                            <ul>
-                                {seznam.map((item) => {
-                                    const { id, name, content } = item;
-                                    return (
-                                        <li key={id}>{name} <br />{content}</li>
-                                    );
-                                })}
-                            </ul>
-                        </div>
-                    </div>
-                );
-            })}
+        <div key={id}>
+            {img ? <Image src={img} alt={title} width={350} height={350} /> : null}
+            <div>
+                <h2>{title}</h2>
+                <p>{content}</p>
+                {seznam && (
+                    <ul>
+                        {seznam.map((item) => (
+                            <li key={item.id}>
+                                {item.name} <br />
+                                {item.content}
+                            </li>
+                        ))}
+                    </ul>
+                )}
+
+            </div>
         </div>
-    )
+
+    );
 }
 
 export default BasicContent
